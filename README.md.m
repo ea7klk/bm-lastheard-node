7c188463f01b9cce68c56266c8ca5e94d950da68
Volker Kerkhoff <volker@kerkhoff.es>
2024-08-24 14:19:09 +0200

    Updated to README

--- README.md.m
+++ README.md.m
@@ -1,13 +1,22 @@
 # Project Name
 
 ## Description
-A brief description of your project.
+This is a NodeJS program, developed almost completely with GitHub Copilot that could be useful for radio amateurs using the Brandmeister DMR network. 
+
+It connect to the BM Lastheard websocket.  
+It gets all the data, ignores non-group calls and calls shorter than 5 seconds.  
+It inserts this data into an SQLITE database.  
+I am also providing some simple web pages rto display some of the data
 
 ## Prerequisites
 - Docker
 - Node.js v22.5.1
 
 ## Installation
+
+Clone the repository. 
+run ```npm install``` to install the dependencies.
+run  ```node lh.js``` to start the program.
 
 ### Clone the repository
 ```bash
